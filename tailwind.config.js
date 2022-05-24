0286936d1b30b55cf460d1da53613825b7bd5df8
Fraud47 <sojandem@gmail.com>
2022-05-24 17:10:35 +0200

    kimberly

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+  
+    extend: {
+      colors: {
+        fblue: '#0F77C6'
+      },
+      
+    },
+    container:{
+      center: true,
+    }
+  },
+  plugins: [],
+}
